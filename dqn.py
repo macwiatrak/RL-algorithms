4d1c471933fffbc7f1626c02b9b74446fd7d2911
Maciek Wiatrak <maciej.wiatrak.16@ucl.ac.uk>
2019-07-14 01:53:37 +0100

    added dqn

--- dqn.py
+++ dqn.py
@@ -6,10 +6,11 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 # hyperparams
-lr = 0.0005
+learning_rate = 0.0005
 gamma = 0.98
 buffer_limit = 50000
 batch_size = 32
+
 
 class ReplayBuffer():
     def __init__(self):
@@ -39,7 +40,7 @@ class Qnet(nn.Module):
         out = self.forward(obs)
         coin = random.random()
         if coin < epsilon:
-            return random.randint(0,1)
+            return random.randint(0, 1)
         else:
             return out.argmax().item()
 
@@ -60,7 +61,7 @@ def train(q, q_target, memory, optimizer):
                                       torch.tensor(r_lst), torch.tensor(s_prime_lst, dtype=torch.float), \
                                       torch.tensor(done_mask_lst)
         q_out = q(s)
-        q_a = q_out.gather(1,a)
+        q_a = q_out.gather(1, a)
         max_q_prime = q_target(s_prime).max(1)[0].unsqueeze(1)
         target = r + gamma * max_q_prime * done_mask
         loss = F.smooth_l1_loss(target, q_a)
@@ -78,18 +79,19 @@ def main():
 
     print_interval = 20
     score = 0.0
-    optimizer = optim.Adam(q.parameters(), lr=lr)
+    optimizer = optim.Adam(q.parameters(), lr=learning_rate)
 
     for n_epi in range(10000):
-        epsilon = max(0.01, 0.09 - 0.01*(n_epi/200)) # linear annealing
+        epsilon = max(0.01, 0.08 - 0.01 * (n_epi / 200))  # Linear annealing from 8% to 1%
         s = env.reset()
 
         for t in range(600):
             a = q.sample_action(torch.from_numpy(s).float(), epsilon)
             s_prime, r, done, info = env.step(a)
-            done_mask =  0.0 if done else 1.0
-            memory.put((s, a, r/100.0, done_mask))
+            done_mask = 0.0 if done else 1.0
+            memory.put((s, a, r / 100.0, s_prime, done_mask))
             s = s_prime
+
             score += r
             if done:
                 break
@@ -97,12 +99,11 @@ def main():
         if memory.size() > 2000:
             train(q, q_target, memory, optimizer)
 
-        if n_epi%print_interval == 0 and n_epi != 0:
+        if n_epi % print_interval == 0 and n_epi != 0:
             q_target.load_state_dict(q.state_dict())
-            print("# of episode : {}, avg score : {:.1f}, buffer size : {}, epsilon : {:.1f}%".format(
-                                                    n_epi, score/print_interval, memory.size(), epsilon*100))
+            print("# of episode :{}, avg score : {:.1f}, buffer size : {}, epsilon : {:.1f}%".format(
+                n_epi, score / print_interval, memory.size(), epsilon * 100))
             score = 0.0
-
     env.close()
 
 
